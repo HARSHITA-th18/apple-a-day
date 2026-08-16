ef36a68c65bc6d57f71f2b395ab322b943e968f9
Tanreet Sandhu <reetsandhu6055@gmail.com>
2026-08-16 18:37:35 +0530

    Update Navbar.jsx

--- src/components/user/Navbar.jsx
+++ src/components/user/Navbar.jsx
@@ -1,5 +1,4 @@
-// import { Link } from "react-router-dom";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function Navbar() {
